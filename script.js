4f877f1179973b418e7e84baf7e05d4dc2e3c507
HammadAli132 <alihammad132@outlook.com>
2024-06-11 17:38:04 +0500

    Done with user options and forms. Only grid is left

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const date = document.getElementById("date");
 date.innerText = new Date().getFullYear();
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+const mainHeight = header.offsetHeight + footer.offsetHeight;
+main.style.cssText = `height: calc(100vh - ${mainHeight}px);`;
