783635ab865fe62779dd0b60f2db58914b4b2066
HammadAli132 <alihammad132@outlook.com>
2024-06-12 19:43:27 +0500

    Removed mouseenter bug for mobiles

--- script.js
+++ script.js
@@ -151,6 +151,7 @@ function startGameWithAI() {
                     }
                 }
             }
+            box.style.background = "var(--primary-color)";
         });
     });
 }
@@ -200,6 +201,7 @@ function startGameWithPlayer2() {
                     return;
                 }
             }
+            box.style.background = "var(--primary-color)";
         });
     });
 }
